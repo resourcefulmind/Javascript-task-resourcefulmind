9fdaa81d01551bbdb1a303d27cfcb8b9c5f60b85
resourcefulmind <61961225+resourcefulmind@users.noreply.github.com>
2020-03-27 08:07:13 -1200

    Create resourcefulmindjavascripttask.js
    First Task on the Javascript class

---
+++ resourcefulmindjavascripttask.js
@@ -0,0 +1,11 @@
+<script>
+    var myName;
+    myName = 'Omodara_Opeyemi';
+    console.log(myName)
+    var courses = ["HTML", "CSS", "Javascript"];
+    console.log(courses);
+    var myArray = [];
+    for (var i = 1; i <= 200; i += 2) {
+        myArray.push(i);
+    }
+</script>
